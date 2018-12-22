a9a16ec2be32a4a6c079ab6d12dc2296dc30db42
Asuk Kono <asukiaaa@gmail.com>
2018-12-22 19:30:56 +0900

    Update require and indent style

--- src/oneframe.clj
+++ src/oneframe.clj
@@ -1,13 +1,13 @@
 (ns oneframe
   (:require
-    [opencv4.core :refer :all]
-    [opencv4.video :as v]))
+   [opencv4.core :as origami]
+   [opencv4.video :as v]))
 
-(defn -main[ & args]
-    (let [capture (v/new-videocapture) device 0 buffer (new-mat)]
-        (doto capture
-         (.open device)
-         (.read buffer)
-         (.release))
-    (prn :camera-image-size (cols buffer) (rows buffer))
-    (imwrite buffer "oneshot.png")))
+(defn -main [& args]
+  (let [capture (v/new-videocapture) device 0 buffer (origami/new-mat)]
+    (doto capture
+      (.open device)
+      (.read buffer)
+      (.release))
+    (prn :camera-image-size (origami/cols buffer) (origami/rows buffer))
+    (origami/imwrite buffer "oneframe.png")))
--- src/webcam.clj
+++ src/webcam.clj
@@ -1,15 +1,14 @@
 (ns webcam
-  (:require
-    [opencv4.core :refer :all]
-    [opencv4.video :as v]
-    [opencv4.utils :as u]))
+  (:require [opencv4.core :as origami]
+            [opencv4.video :as v]
+            [opencv4.utils :as u]))
 
-(defn -main[ & args]
+(defn -main [& args]
   (u/simple-cam-window
-    (fn [buffer]
-    (u/resize-by buffer 0.3)
-    (let [ output (new-mat) bottom (-> buffer clone (flip! -1)) ]
-     (-> buffer (cvt-color! COLOR_RGB2GRAY) (cvt-color! COLOR_GRAY2RGB))
-     (put-text buffer (str (java.util.Date.)) (new-point 10 50) FONT_HERSHEY_PLAIN 1 (new-scalar 255 255 0) 1)
-     (vconcat [buffer bottom] output)
-     output))))
+   (fn [buffer]
+     (u/resize-by buffer 0.3)
+     (let [output (origami/new-mat) bottom (-> buffer origami/clone (origami/flip! -1))]
+       (-> buffer (origami/cvt-color! origami/COLOR_RGB2GRAY) (origami/cvt-color! origami/COLOR_GRAY2RGB))
+       (origami/put-text buffer (str (java.util.Date.)) (origami/new-point 10 50) origami/FONT_HERSHEY_PLAIN 1 (origami/new-scalar 255 255 0) 1)
+       (origami/vconcat [buffer bottom] output)
+       output))))
